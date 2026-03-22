624e297307b5f3bf21ccd8fb8853b656a3e9f395
Local Developer <projects@local.dev>
2026-03-22 18:04:43 +0530

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    const float timelimit = 0.5f;
+    float timeSinceLastDog = 0.0f;
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+
+        timeSinceLastDog += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && timeSinceLastDog>timelimit)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeSinceLastDog = 0.0f;
+        }
+    }
+}
